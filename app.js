58c6720714b5b6dd26b5605cdc2b3ff3d7e0b5cd
Varundhyani69 <varundhyani29@gmail.com>
2025-04-18 22:28:03 +0530

    Update and Edit functionality

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
+const methodOverride = require("method-override");
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/bnb';
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 main().then(()=>{
     console.log("connected to db");
@@ -44,7 +46,18 @@ app.post("/listings",async(req,res)=>{
     res.redirect("/listings");
     console.log(listing);
 })
-
+//Edit Route
+app.get("/listings/:id/edit",async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("./listings/edit.ejs",{listing});
+})
+//Update Route
+app.put("/listings/:id",async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
 
 // app.get("/testListing",async (req,res)=>{
 //     let sampleListing = new Listing({
