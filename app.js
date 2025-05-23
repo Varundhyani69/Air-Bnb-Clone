25bd32595bca928fd6c638870d0426a164f6d376
Varundhyani69 <varundhyani29@gmail.com>
2025-05-23 23:13:35 +0530

    Express-Route

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
-const path = require("path");
+const mongoose = require("mongoose");const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
-const ExpressError = require("./utils/ExpressError.js")
-const {listingSchema ,reviewSchema} = require("./schema.js");
-const Review = require("./models/review.js");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -33,104 +30,17 @@ app.get("/",(req,res)=>{
     res.send("Hi I am root");
 })
 
-const validListing = (req,res,next)=>{
-    let {error} = listingSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    }else{
-        next();
-    }
-}
 
-const validReview = (req,res,next)=>{
-    let {error} = reviewSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    }else{
-        next();
-    }
-}
 
-//index route
-app.get("/listings",wrapAsync(async (req,res)=>{
-    const allListings = await Listing.find({});
-    res.render("./listings/index.ejs",{allListings});
-}))
-//new route
-app.get("/listings/new",(req,res)=>{
-    res.render("./listings/new.ejs");
-})
-//show route
-app.get("/listings/:id",wrapAsync(async(req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs",{listing});
-}))
-//create route
-app.post("/listings", validListing, wrapAsync(async (req,res,next)=>{
-        
-        const newListing = new Listing(req.body.listing);
-        
-        await newListing.save(); 
-        res.redirect("/listings");
-    })
-)
-//edit route
-app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("./listings/edit.ejs",{listing});
-}))
-//update route
-app.put("/listings/:id",validListing,wrapAsync(async (req,res)=>{
-    if(!req.body.listing){
-            throw new ExpressError(400,"Send Valid Data!");
-        }
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect("/listings");
-}))
-//delete route
-app.delete("/listings/:id",wrapAsync(async(req,res)=>{
-    let {id} = req.params;
-    await Listing.findByIdAndDelete(id);
-    res.redirect("/listings");
-}))
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews);
 
-//Review Post Route
-app.post("/listings/:id/reviews",validReview,wrapAsync(async(req,res)=>{
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    listing.reviews.push(newReview);
-    await newReview.save();
-    await listing.save();
-    // res.send("New Review Saved");
-    res.redirect(`/listings/${listing._id}`);
-}))
-
-//Delete review
-app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
-    let {id,reviewId} = req.params;
-    await Listing.findByIdAndUpdate(id,{$pull: {reviews:reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/listings/${id}`);
-}))
-
-// Catch-all route for undefined paths
-// app.all("*", (req, res, next) => {
-//     next(new ExpressError(404, "Page Not Found"));
-// });
 
 // Error-handling middleware
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = "Something went wrong!" } = err;
     res.status(statusCode).render("error.ejs", { err });
 });
-
-
-
 
 app.listen(8080,()=>{
     console.log("Server is listening to 8080");
